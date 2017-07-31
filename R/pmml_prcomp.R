48efdefbf05ad7dd49676a10462661c65e82ebb6
alex23lemm <alex23lemm@gmail.com>
2017-07-31 13:50:05 +0200

    Create private function to capture prcomp pre-processing

--- R/pmml_prcomp.R
+++ R/pmml_prcomp.R
@@ -1,7 +1,7 @@
-#' Generates PMML for prcomp objects
+#' Generate PMML for prcomp objects
 #'
-#' Extracts one principal component from a prcomp object together with the
-#' centering and scaling information and generates the PMML representation.
+#' Extract one principal component from a prcomp object together with the
+#' centering and scaling information and generate the PMML representation.
 #'
 #' In general, each principal component represents a linear combination of the
 #' original input values. Therefore, the information of a single principal
@@ -19,51 +19,85 @@
 #' the "minimal" lm object are passed to \code{pmml.lm()} to create the PMML.
 #'
 #' @param model A prcomp object.
-#' @param n The number of the principal component for which the PMML will be created.
+#' @param j Index of the eigenvector for which the PMML will be created.
 #'  Per default, the PMML will be constructed based on the eigenvector of the
 #'  first principal component.
-#' @param model.name	A name to be given to the model in the PMML code.
-#' @param description	A descriptive text for the Header element of the PMML code.
 #' @param ... Further arguments passed to \code{pmml.lm()} which make sense in
 #'  the given context
 #'
-#' @return An object of class XMLnode which is of type PMML.
+#' @return An object of class XMLNode which is of type PMML.
 #' @export
-pmml_prcomp <- function(model, n = 1, model.name = "Principal_Component_Model",
-                        description = "Principal Component Model") {
+#' @examples
+#' iris <- iris[, -5]
+#' pc_iris <- prcomp(iris, center = FALSE, scale. = FALSE)
+#' pmml_prcomp(pc_iris)
+#'
+#' pc_iris <- prcomp(iris, center = TRUE, scale. = TRUE)
+#' pmml_prcomp(pc_iris, 2)
+pmml_prcomp <- function(model, j = 1, ...) {
+  if(class(model) != "prcomp")
+    stop("Please provide a prcomp object")
 
-  empty_df<- data.frame(matrix(data = rep(0, ncol(model$rotation) + 1),
-                               nrow = 1, ncol = ncol(model$rotation) + 1))
-  names(empty_df) <- c(row.names(model$rotation), paste0("PC", n))
-  empty_box <- pmmlTransformations::WrapData(empty_df)
+  if (j > ncol(model$rotation))
+    stop("Please specify a valid eigenvector index")
 
-  origFieldName <-  row.names(model$rotation)
-  newFieldName <- paste0(origFieldName, "_transformed")
-  center <- model$center
-  scale <- model$scale
+  empty_box = NULL
+  field_names <- row.names(model$rotation)
 
-  # Add centering and scaling as pre-processing steps to the WrapData object
-  for(i in 1:nrow(model$rotation)) {
-    empty_box <- pmmlTransformations::FunctionXform(empty_box,
-                                                    origFieldName = origFieldName[i],
-                                                    newFieldName = newFieldName[i],
-                                                    formulaText = paste0("(", origFieldName[i], "-",
-                                                                  center[i], ")", "/", scale[i]))
+  if (typeof(model$center) != "logical" || typeof(model$scale) != "logical") {
+    field_names <- paste0(field_names, "_transformed")
+    empty_box <- preprocess_prcomp(model$rotation[, j], model$center,
+                                   model$scale, j)
   }
 
-  # Extract principal component coeffients
-  pc_coef <- c("(Intercept)" = 0, model$rotation[, n])
-  # Important to overwrite the original names with their "_transformed" version
-  # so that adding of the WrapData object to pmml.lm() will work
-  names(pc_coef)[2:length(pc_coef)] <- newFieldName
+  pc_coef <- c("(Intercept)" = 0, model$rotation[, j])
+
+  names(pc_coef)[2:length(pc_coef)] <- field_names
   dataClasses <- rep("numeric", length(pc_coef))
   names(dataClasses) <- names(pc_coef)
-  names(dataClasses)[1] <- paste0("PC", n)
+  names(dataClasses)[1] <- paste0("PC", j)
 
   fit <- list(coefficients = pc_coef, call = call("lm"), terms = NULL)
   attributes(fit$terms) <- list(dataClasses = dataClasses)
   class(fit) <- "lm"
 
-  pmml <- pmml::pmml.lm(fit, model.name, description, transforms = empty_box,
+  pmml <- pmml::pmml.lm(fit, transforms = empty_box,
                         ...)
+  pmml
+}
+
+
+#' Create preprocessing steps for prcomp objects
+#'
+#' @param eigenvector	Eigenvector containing variable names
+#' @param center The centering used, or FALSE
+#' @param scale The scaling used, or FALSE
+#' @param j Selected eigenvector index
+#'
+#' @return A WrapData object containing the preprocessing steps
+preprocess_prcomp <- function(eigenvector, center, scale, j) {
+  empty_df<- data.frame(matrix(data = rep(0, length(eigenvector) + 1),
+                               nrow = 1, ncol = length(eigenvector) + 1))
+  names(empty_df) <- c(names(eigenvector), paste0("PC", j))
+  empty_box <- pmmlTransformations::WrapData(empty_df)
+
+  origFieldName <-  names(eigenvector)
+  newFieldName <- paste0(origFieldName, "_transformed")
+
+
+  if (typeof(center) != "logical" && typeof(scale) == "logical")
+    formulaText <- quote(paste0(origFieldName[i], "-", center[i]))
+  else if (typeof(center) == "logical" && typeof(scale) != "logical")
+    formulaText <- quote(paste0(origFieldName[i], "/", scale[i]))
+  else
+    formulaText <- quote(paste0("(", origFieldName[i], "-", center[i], ")", "/",
+                                scale[i]))
+
+  for(i in seq_along(eigenvector)) {
+    empty_box <- pmmlTransformations::FunctionXform(empty_box,
+                                                    origFieldName = origFieldName[i],
+                                                    newFieldName = newFieldName[i],
+                                                    formulaText = eval(formulaText))
+  }
+  empty_box
 }
